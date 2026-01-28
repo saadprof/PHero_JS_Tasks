c9fb603bec54a0446f4ce2ba0300913bf3503f23
saadProf <saadprof101@gmail.com>
2026-01-28 17:25:13 +0600

    Description and examples of comparison operators added.

---
+++ comparison_operators.js
@@ -0,0 +1,39 @@
+/*
+    Camparison operators returns boolean value.
+    Comparison Operators: 
+        Greate than >, 
+        Less than <, 
+        Equals to ==, 
+        Greater than or equals to >=, 
+        Less than or equals to <=,
+        Not equals to !=
+*/ 
+
+let a = 20;
+let b = 30;
+let c = 25;
+let d = 30;
+
+// Example of Greate than >:
+console.log("'b' is greater than 'a' ->", b > a);
+console.log('------------------------------------');
+
+// Example of Less than <:
+console.log("'b' is less than 'c' ->", b < c);
+console.log('------------------------------------');
+
+// Example of Equals to ==:
+console.log("'b' is equals to 'd' ->", b == d);
+console.log('------------------------------------');
+
+// Example of Greater than or Eqals to >=:
+console.log("'d' is greater than or equals to 'b'", d >= b);
+console.log('------------------------------------');
+
+// Example of Less than or Equals to <=:
+console.log("'c' is less than or equals to 'd' ->", c <= d);
+console.log('------------------------------------');
+
+// Example of not equals to:
+console.log("'b' is not equlas to 'c' ->", b != c);
+console.log('------------------------------------');
