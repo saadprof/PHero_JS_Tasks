f4b4824696053e39d2a20e22341c4b2c2c635461
saadProf <saadprof101@gmail.com>
2026-01-28 11:18:30 +0600

    Description of Arithmatic operation and examples added.

---
+++ arithmatic_operation_shorthands.js
@@ -0,0 +1,37 @@
+/*
+    Arithmatic Operators:
+        i. Addition: Binary plus sign '+' with two operands perform addition.
+            i.e -> 2 + 3
+        ii. Subtraction: Binary minus  sign '-' with two operands perform subtraction from the first value
+            i.e -> 10 - 5
+        iii. Multiplication: Binary multiply sign '*' with two operands perform multiplication.
+            i.e -> 5 * 5
+        iv. Division: Binary division sign '/' performs division.
+            i.e -> 10 / 5
+        v. Modulas/Remainder: Modulas or Remainder sign returns the remainder after division. 
+*/ 
+
+// Addition Example:
+let add = 2 + 3;
+console.log(add);
+console.log('------------------------------------');
+
+// Subtraction Example:
+let subtract = 10 - 5;
+console.log(subtract);
+console.log('------------------------------------');
+
+// Multiplication Example:
+let multiply = 5 * 5;
+console.log(multiply);
+console.log('------------------------------------');
+
+// Division Example:
+let devide = 10 / 5 ;
+console.log(devide);
+console.log('------------------------------------');
+
+// Remainder Example:
+let remainder = 50 % 9;
+console.log(remainder);
+console.log('------------------------------------');
