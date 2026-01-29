1437a208fdb85e05b14126d88289db3e10fb9403
saadProf <saadprof101@gmail.com>
2026-01-29 12:01:39 +0600

    Multilevel condition statement with many different values.

---
+++ multilevel_condition.js
@@ -0,0 +1,26 @@
+/*
+    Here we'll be using broad example of if...else if...else block of conditional statement.
+*/ 
+
+let bigBroAge = 32;
+let bigBroCars = 4;
+let bigBroMonthlyDollah = 20000;
+let bigBroName = "Tomahawk";
+let aUGa = false;
+let ballsCount = 1;
+let bigBroFit = false;
+
+
+if(bigBroAge > 30 && bigBroFit){
+    console.log("Big Bro Breaths OxyGen.");
+} else if(bigBroCars > 4 || bigBroMonthlyDollah > 20000){
+    console.log("Bros richy.");
+} else if(aUGa){
+    console.log("Wy U a ga!");
+} else if(ballsCount < 2 && ballsCount > 2){
+    console.log("Watcha ur bowls.");
+} else{
+    console.log("I'm Grootamahawk");
+}
+
+// Twiking upper values will effect on these conditional results.
