1ffadac9f81563405e9c23301c57336723732e7b
saadProf <saadprof101@gmail.com>
2026-02-01 15:15:53 +0600

    Ternary operator description and basic example is added.

---
+++ ternary_operator.js
@@ -0,0 +1,10 @@
+/*
+    Ternary operator is the comprehensive & short version of conditional statments.
+    Here's the syntax:
+                        (statement) ? executes if statement is true : executes if statement is false
+*/ 
+
+// Basic ternary example:
+const maxGovJobAge = 32;
+let myAge = 33;
+myAge > maxGovJobAge ? console.log("Oh Pops! Watch yur knee grows") : console.log("Take this lolijob lil fella.");
