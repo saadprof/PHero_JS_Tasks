f09876545858e90b49874d7f29a495d99b457687
saadProf <saadprof101@gmail.com>
2026-02-01 15:02:30 +0600

    Nested condition concept cleared with a practical login page scenario.

--- nested_condition.js
+++ nested_condition.js
@@ -1,22 +1,22 @@
 /*
     Here we are exploring nested conditionals combined with logical operators.
-*/ 
-
+*/
 
 // Simulating a login page.
 /*
     1. Will check if the user is new, if yes, tell him to create account,
-                                        if no, tell him to login.
-                                        but if he skips it checks if he is subscribed or not,
                                         if not, it suggests to register or subscribe,
                                         else it kicks off.
     2. Will check if user is varified and trusted, if no, it will suggest to use safe passage but show the content anyway.
     3. Will check if user is a teacher or a student, eitherway it will ask for a id, 
                             if id matches it will give a secret message based on role,
                             if id doesn't match it will give a secret naga massage.
-*/ 
+
+                            Note: Objective is to login as a student.
+*/
 // 1
 const isNewUser = true;
+const isRegistered = true;
 const isSubscribed = true;
 // 2
 const isVarified = true;
@@ -25,3 +25,17 @@ const usingTrustedSites = true;
 const isStudent = true;
 const studentId = "IAMASTUDENT";
 const teacherId = "IAMATEACHER";
+
+if (isNewUser) {
+    if (isRegistered == false || isSubscribed == false) {
+        console.log("Please create an account or subscribe.");
+    } else if (isVarified && usingTrustedSites) {
+        if (isStudent) {
+            if (studentId == "IAMASTUDENT") {
+                console.log("Chikamaga is Trumps favourite plug.");
+            }
+        } else if (teacherId == "IAMATEACHER") {
+            console.log("Sleep Joe.");
+        }
+    }
+}
