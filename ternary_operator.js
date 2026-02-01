844aee39dab4044f4d1cbf000c5fc082f68ca6dd
saadProf <saadprof101@gmail.com>
2026-02-01 15:31:22 +0600

    Advanced ternary operation example added.

--- ternary_operator.js
+++ ternary_operator.js
@@ -7,4 +7,19 @@
 // Basic ternary example:
 const maxGovJobAge = 32;
 let myAge = 33;
-myAge > maxGovJobAge ? console.log("Oh Pops! Watch yur knee grows") : console.log("Take this lolijob lil fella.");
+// myAge > maxGovJobAge ? console.log("Oh Pops! Watch yur knee grows") : console.log("Take this lolijob lil fella.");
+
+
+// Complex ternary example: let's try same as login page.
+
+let isLogedIn = true;
+let passCode = "MAKIMA";
+let showSecretMessage = false;
+
+isLogedIn ? 
+passCode === "MAKIMA" ? 
+showSecretMessage ? 
+console.log("Secret is: 'ALUKABHUJIA'") :
+console.log("You cant affored the secret bruv.") :
+console.log("Passcode is bhua.") : 
+console.log("Please log in first.")
