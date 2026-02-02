c8f563cb34c07ed5169d400b1516568d1d557ac8
saadProf <saadprof101@gmail.com>
2026-02-02 10:52:28 +0600

    The burger discount programm is rewritten and now it's working. Not full foolprofed but it works.

--- js_conditional_tasks.js
+++ js_conditional_tasks.js
@@ -1,3 +1,5 @@
+
+/*-----------  Burger Purchase Discount Calculator --------------*/
 /*
     Discount calculator for a returant. 
     Offers are on: 1. burger purchase ammounting over 500 tk will get 2 free coke.
@@ -15,13 +17,22 @@ let burgerQuantity = 3;
 let combo1Quantity = 0;
 let combo2Quantity = 1;
 
-if(burgerQuantity * burgerPrice > 500){
-    console.log("You'll get 2 free coke with the purchase sir.");
+// if(burgerQuantity * burgerPrice > 500){
+//     console.log("You'll get 2 free coke with the purchase sir.");
+// } else if((combo1Quantity * combo1Price) > 1000 || (combo2Quantity * combo2Price) > 1000){
+//     console.log("You'll get 2 free coke and a free french coke sir.")
+// } else if((burgerQuantity * burgerPrice) > 500 && ((combo1Quantity * combo1Price) > 1000 || (combo2Quantity * combo2Price) > 1000)){
+//     console.log("Throwing a party or something. Here you go with a 5 coke and a french fry on the house.");
+// } else if(burgerQuantity < 3){
+//     console.log("Wanna get a coke? It'll cost only 25 taka.");
+// }
+
+if((burgerQuantity * burgerPrice) > 500 && ((combo1Quantity * combo1Price) > 1000 || (combo2Quantity * combo2Price) > 1000)){
+    console.log("Throwing a party or something. Here you go with a 5 coke and a french fry on the house.");
 } else if((combo1Quantity * combo1Price) > 1000 || (combo2Quantity * combo2Price) > 1000){
     console.log("You'll get 2 free coke and a free french coke sir.")
-} else if((burgerQuantity * burgerPrice) > 500 && ((combo1Quantity * combo1Price) > 1000 || (combo2Quantity * combo2Price) > 1000)){
-    console.log("Throwing a party or something. Here you go with a 5 coke and a french fry on the house.");
+} else if(burgerQuantity * burgerPrice > 500){
+    console.log("You'll get 2 free coke with the purchase sir.");
 } else if(burgerQuantity < 3){
     console.log("Wanna get a coke? It'll cost only 25 taka.");
 }
-
