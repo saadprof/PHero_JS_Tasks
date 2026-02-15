f4014f4fc4f5d2d6fe22ed2ce16140d34e06ffa1
saadProf <saadprof101@gmail.com>
2026-02-15 20:22:13 +0600

    Basic description of loops are added.

---
+++ about_loops.js
@@ -0,0 +1,20 @@
+/*
+    There are three types of loop in JavaScript.
+        1. while loop: While loop executes while the condition is true. Syntax:
+        let initiator    
+        while(condition){
+                code;
+                increament/decrement;
+        }
+        
+        2. for loop: For loop is a shorthand and compact version of while loop. It combines three components of loop (initiator, condition, increamentor/decreamentor).
+            Syntax: for(let initiator; condition; increament/decreament){
+                    code;
+                }
+        
+        3. do while loop: Do while loop is similar to while loop but the loop always run once before checking the condition. Syntax:
+            do{
+                code;
+                increament/decreament;
+            } while (condition)
+*/ 
