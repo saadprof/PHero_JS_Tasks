432ca0d501bf983f6850d7b0826a9611f467d541
saadProf <saadprof101@gmail.com>
2026-02-15 16:08:42 +0600

    Operation using both if-else and ternary operator completed.

--- js_conditional_tasks.js
+++ js_conditional_tasks.js
@@ -114,3 +114,29 @@ if(myResult >= 80){
 } else{
     console.log("Well, I'm off myself now.");
 }
+
+
+
+/* ---------------------- Ternary Operator Output ------------------*/
+/*
+you have two numbers in two variables, called: num1, num2
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. 
+if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. also, write it using ternary operator.
+*/ 
+let num1 = 55;
+let num2 = 68;
+let sresult;
+
+// Doing this task using if-else
+
+    // if(num1 > num2){
+    //     sresult = num1 * 2
+    // } else{
+    //     sresult = num1 + num2
+    // }
+
+num1 > num2 ? sresult = num1 * 2 : sresult = num1 + num2;
+console.log("The result is: ", sresult);
